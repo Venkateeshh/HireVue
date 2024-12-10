6e74b5cb2e17b06bd3458218971de30c9bf1db3a
Venkatesh <rkamathvenkatesh@gmail.com>
2024-12-10 11:48:35 +0530

    feat: update dependencies and modify video call parameters

--- client/src/components/video/video-call.tsx
+++ client/src/components/video/video-call.tsx
@@ -13,12 +13,12 @@ import {
 import "@stream-io/video-react-sdk/dist/css/styles.css"
 import "./styles.css"
 
-const apiKey = 'mmhfdzb5evj2';
-const callId = 'BiGNZTxPgu4Y';
+const apiKey = "mmhfdzb5evj2"
+const callId = "3AYaWhE7J9YI"
 
 export default function VideoCal() {
     const currentUser = useVideoCall() // Hook called inside the component
-    const userId = 'Jarael';
+    const userId = "Anakin_Solo"
 
     const user: User = {
         id: userId, // Ensure this matches `user_id` in token
@@ -26,7 +26,8 @@ export default function VideoCal() {
         image: `https://api.dicebear.com/6.x/adventurer/svg?seed=${currentUser.username || "user"}`, // Generate avatar
     }
 
-    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL0phcmFlbCIsInVzZXJfaWQiOiJKYXJhZWwiLCJ2YWxpZGl0eV9pbl9zZWNvbmRzIjo2MDQ4MDAsImlhdCI6MTczMzM3MTk3MSwiZXhwIjoxNzMzOTc2NzcxfQ.SXNT-MWfpAbdDlx1RnsaKK_uLeucu3uKiF2XyPyOp5o';
+    const token =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL0FuYWtpbl9Tb2xvIiwidXNlcl9pZCI6IkFuYWtpbl9Tb2xvIiwidmFsaWRpdHlfaW5fc2Vjb25kcyI6NjA0ODAwLCJpYXQiOjE3MzM3OTY1NDUsImV4cCI6MTczNDQwMTM0NX0.tKL7DIZGaLeNlOM-DenBCL7Ss6atO_e8MRdEivjRobI"
 
     const client = new StreamVideoClient({ apiKey, user, token })
     const call = client.call("default", callId)
